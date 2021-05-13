5f3af65e10d155aa54bddcb881dd6904546a2f46
AnanthaBharadwaj <atgsb99@gmail.com>
2021-05-13 12:13:01 +1000

    Initial Push

---
+++ app/src/main/java/com/example/supertradermarkets/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.supertradermarkets
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+
+// This activity gets called when the app is launched
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Change URL here for webview here
+        val mWebView : WebView = findViewById<View>(R.id.superWeb) as WebView
+        mWebView.loadUrl("https://portal.supertraderfx.com/")
+
+        val webSettings : WebSettings = mWebView.settings
+        webSettings.javaScriptEnabled = true
+        mWebView.webViewClient = WebViewClient()
+
+        mWebView.canGoBack()
+        mWebView.setOnKeyListener { v, keyCode, event ->
+
+            if (keyCode  == KeyEvent.KEYCODE_BACK && event.action == MotionEvent.ACTION_UP && mWebView.canGoBack()) {
+                mWebView.goBack()
+                return@setOnKeyListener true
+            }
+            false
+        }
+    }
+}
